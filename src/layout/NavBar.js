80bd0969edebfec7675461383f4a52c790e34ff6
LeeWannacott <WannacottL@gmail.com>
2024-12-25 14:37:45 +1300

    Update name on portfolio.

--- src/layout/NavBar.js
+++ src/layout/NavBar.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
-import { Link, Route,  Switch, } from "react-router-dom";
-import {
-  Nav,
-  Navbar,
-} from "react-bootstrap";
+import { Link, Route, Switch } from "react-router-dom";
+import { Nav, Navbar } from "react-bootstrap";
 import "./Navbar.css";
 import Home from "./Home";
 import Projects from "./Projects";
@@ -13,48 +10,48 @@ import Blog from "./Blog";
 // import LinearProgress from '@material-ui/core/LinearProgress';
 
 export class NavBar extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Navbar className="nav-color" expand="lg" variant="dark">
-          <Navbar.Brand>Lee Wannacott</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link className="nav-link" as={Link} to="/Blog">
-                {" "}
-                Blog{" "}
-              </Nav.Link>
-              {/*<Nav.Link as={Link} to="/Projects">
+	render() {
+		return (
+			<div className="container">
+				<Navbar className="nav-color" expand="lg" variant="dark">
+					<Navbar.Brand>Kyle Wannacott</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse id="basic-navbar-nav">
+						<Nav className="mr-auto">
+							<Nav.Link className="nav-link" as={Link} to="/Blog">
+								{" "}
+								Blog{" "}
+							</Nav.Link>
+							{/*<Nav.Link as={Link} to="/Projects">
                 {" "}
                 Projects{" "}
               </Nav.Link>
               */}
-              <Nav.Link as={Link} to="/GitHub">
-                {" "}
-                Github Statistics{" "}
-              </Nav.Link>
-              <Nav.Link className="nav-link" as={Link} to="/Contact">
-                {" "}
-                Contact{" "}
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
-        {/* <LinearProgress/> */}
-        <br></br>
-        <div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/Blog" component={Blog} />
-            <Route path="/Projects" component={Projects} />
-            <Route path="/GitHub" component={GitHub} />
-            <Route path="/Contact" component={Contact} />
-          </Switch>
-        </div>
-      </div>
-    );
-  }
+							<Nav.Link as={Link} to="/GitHub">
+								{" "}
+								Github Statistics{" "}
+							</Nav.Link>
+							<Nav.Link className="nav-link" as={Link} to="/Contact">
+								{" "}
+								Contact{" "}
+							</Nav.Link>
+						</Nav>
+					</Navbar.Collapse>
+				</Navbar>
+				{/* <LinearProgress/> */}
+				<br></br>
+				<div>
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/Blog" component={Blog} />
+						<Route path="/Projects" component={Projects} />
+						<Route path="/GitHub" component={GitHub} />
+						<Route path="/Contact" component={Contact} />
+					</Switch>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default NavBar;
